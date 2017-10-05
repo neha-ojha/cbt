bcb21f2191dc33da2103940a21c439fa819a1a33
Neha Ojha <nojha@redhat.com>
2017-10-05 15:23:42 -0700

    wip: comparison with baseline
    Signed-off-by: Neha Ojha <nojha@redhat.com>

--- read_baseline.py
+++ read_baseline.py
@@ -22,17 +22,35 @@ def get_result_files(dir_path):
             fnames.append(fname)
     return fnames
 
-def compare_with_baseline(cbt_dir_path, fname, baseline):
+def compare_parameters(test, baseline):
     ret = 0
-    with open(os.path.join(cbt_dir_path, fname)) as fd:
+    for key in ["bw", "std_dev_bw", "avg_iops", "std_dev_iops", "avg_lat",
+                "std_dev_lat"]:
+        if test[key] < baseline[key]/10:
+            ret = 1
+            logger.info('%s test failed', key)
+        else:
+            logger.info('%s test passed', key)
+    return ret 
+
+def compare_with_baseline(btype, fpath, baseline):
+    ret = 0
+    test_result = {}
+    with open(fpath) as fd:
         result = json.load(fd)
-    logger.info("JSON results: %s", result)
-    logger.info("Output Bandwidth: %s", result["Bandwidth (MB/sec)"])
-    logger.info("Baseline Bandwidth: %s", baseline["bw"])
-    if float(result["Bandwidth (MB/sec)"]) < float(baseline["bw"]):
-        logger.info("Bandwidth test failed")
-        ret = 1
-        raise Exception('Performance test failed')
+    if btype == "radosbench":
+        test_result["bw"] = float(result["Bandwidth (MB/sec)"])
+        test_result["std_dev_bw"] = float(result["Stddev Bandwidth"])
+        test_result["avg_iops"] = float(result["Average IOPS"])
+        test_result["std_dev_iops"] = float(result["Stddev IOPS"])
+        test_result["avg_lat"] = float(result["Average Latency(s)"])
+        test_result["std_dev_lat"] = float(result["Stddev Latency(s)"])
+    logger.info('Baseline values: %s', baseline)
+    logger.info('Test Values: %s', test_result)
+    if btype == "librbdfio":
+        pass
+
+    ret = compare_parameters(test_result, baseline)
     return ret
 
 def main(argv):
@@ -41,22 +59,38 @@ def main(argv):
     results = sys.argv[2]
     with open(config) as fd:
         parameters = yaml.load(fd)
-    logger.info('Baseline Parameters are: %s', parameters["baseline"])
-    logger.info('CBT results directory: %s', results)
-
     iterations = parameters["cluster"]["iterations"]
+    btype = parameters["benchmarks"].keys()[0]
+    logger.info('Starting Peformance Tests for %s', btype)
+    ret_vals = {}
     for iteration in range(iterations):
-        ret = 0
+        logger.info('Iteration: %d', iteration)
         cbt_dir_path = os.path.join(results, '%08d' % iteration)
-        logger.info('CBT dir path: %s', cbt_dir_path)
         result_files = get_result_files(cbt_dir_path)
-        logger.info('Results filenames: %s', result_files)
-        ret = 0
+        failed_test = []
         for fname in result_files:
-            ret = compare_with_baseline(cbt_dir_path, fname, parameters["baseline"])
+            ret = 0
+            logger.info('Running performance test for: %s', fname)
+            fpath = os.path.join(cbt_dir_path, fname)
+            ret = compare_with_baseline(btype, fpath, parameters["baseline"])
             if ret != 0:
-                break
-    return ret
+                failed_test.append(fname)
+        ret_vals[iteration] = failed_test
+        if failed_test:
+            logger.info('Failed tests in iteration %d: %s', iteration, failed_test)
+        else:
+            logger.info('All performance tests passed for iteration: %d', iteration)    
+    
+    # Summary of Performance Tests
+    logger.info('Summary of Performance Tests')
+    failed = 0
+    for iteration in range(iterations):
+        if ret_vals[iteration]:
+            logger.info('Failed performance tests in iteration %d: %s', iteration, ret_vals[iteration])
+            failed = 1
+    if failed == 1:
+        raise Exception('Performance test failed')
+    logger.info('All Performance Tests Succeeded!')
 
 if __name__ == '__main__':
     exit(main(sys.argv))
